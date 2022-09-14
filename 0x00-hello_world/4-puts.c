b4ee64d245726c209cef6a16621c49e98af0be9f
nkirda <danielmshel@gmail.com>
2022-09-14 15:17:25 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -printing 
+ *
+ * return: Alway 0 (succesful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle');
+ return (0);
+}
