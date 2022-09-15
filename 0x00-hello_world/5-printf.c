8c26c319f8a5aed147841f0776d106936797390f
nkirda <danielmshel@gmail.com>
2022-09-14 20:51:33 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  * main - print
  *
  * Return: Alway 0 (succesful c program)
- */                                         int main(void)                              {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
+ */                                         int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
