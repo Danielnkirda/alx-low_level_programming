0512fe7fe27e3e5f6a2462115489a68f79a28c15
nkirda <danielmshel@gmail.com>
2022-09-15 23:44:25 -0700

    case checker

--- 0x04-more_functions_nested_loops/putchar.c
+++ 0x04-more_functions_nested_loops/putchar.c
@@ -1,9 +1,14 @@
 #include <unsistd.h>
 #include "main.h"
+
 /**
- * putchar
+ * -putchar - write the character c to stdout
+ *  @c: The character to print
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and erno is set appropraitely.
  */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
